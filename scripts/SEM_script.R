f951ded9d9839974aeb45ddc641674a13ef0d03d
lm-coelho <l.m.corredoura.coelho@student.rug.nl>
2024-11-20 16:46:25 +0100

    model 3

--- scripts/SEM_script.R
+++ scripts/SEM_script.R
@@ -1,6 +1,17 @@
 
 renv::restore()
 
+library(terra)       # for working with raster data
+library(tidyterra)   # for adding terra objects to ggplot
+library(ggspatial)  # for scale bars
+library(sf)          # for vector data objects
+library(tidyverse)   # ggplot, dplyr etc
+library(scales)      # for oob (out of bounds) scale
+library(ggnewscale) # for using multiple color fill scales in ggplot
+library(patchwork)  # for combining multiple ggplots in one panel plot
+library(ggplot2)
+
+
 # read the data from the google docs link:
 SEMdata <-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR-cr8z8Pye7gakP0ihX5SUtNLU8h_evJAySe4ZcnRL1d2HAoOkHKmDe9cM6uA4oC1A4QRArIlxFX5V/pub?gid=1240187235&single=true&output=csv")
 names(SEMdata)
@@ -24,8 +35,6 @@ psych::pairs.panels(SEMdata %>% dplyr::select(dist2river,elevation,rainfall,vall
 psych::pairs.panels(SEMdatastd %>% dplyr::select(dist2river,elevation,rainfall,valleys, burnfreq, 
                                           CEC, PA, woody),
                     stars = T, ellipses = F, cex.labels = 1.5, cex.cor = 2)
-
-
 
 
 
@@ -53,3 +62,36 @@ summary(woody_fit, standardized=T, fit.measures=T, rsquare=T)
 
 #make another model Piecewise!!!
 #(e.g. where Rainfall don't affect woody directly but only CEC/ PA only influencing CEC/ maybe taking valleys out?!)
+
+
+## Selected variables in order to get a better model
+woody_model2 <- 'woody~burnfreq+CEC+valleys
+                rainfall~elevation
+                valleys~dist2river
+                burnfreq~dist2river
+                CEC~rainfall+burnfreq+PA'
+woody_model2
+woody_fit2 <- lavaan::sem(woody_model2, data=SEMdatastd)
+
+# show the model results
+summary(woody_fit2, standardized=T, fit.measures=T, rsquare=T)
+# CFI=0.778 and TLI=0.653 -> BETTER BUT STILL NOT A GOOD MODEL...
+# goodness of fit (should be >0.9): CFI and TLI
+# badness of fit: ( should be <0.1): RMSEA, SRMR
+
+
+
+## Selected RAINFALL AND DISTANCE TO RIVER in order to get a better model
+woody_model3 <- 'woody~burnfreq+CEC+valleys
+                rainfall~elevation+dist2river
+                valleys~dist2river
+                burnfreq~dist2river
+                CEC~rainfall+burnfreq+PA'
+woody_model3
+woody_fit3 <- lavaan::sem(woody_model3, data=SEMdatastd)
+
+# show the model results
+summary(woody_fit3, standardized=T, fit.measures=T, rsquare=T)
+# CFI=0.989 and TLI=0.982 -> GOOD MODEL!!!
+# goodness of fit (should be >0.9): CFI and TLI
+# badness of fit: ( should be <0.1): RMSEA, SRMR
